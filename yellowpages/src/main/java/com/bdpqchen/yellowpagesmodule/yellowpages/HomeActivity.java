99e165400f4c39845ca564eb9528e10e630fcc2a
bdpqchen <bdpqchen@gmail.com>
2017-02-25 20:40:54 +0800

    dataset问题以及支持空列表的问题待解决

--- yellowpages/src/main/java/com/bdpqchen/yellowpagesmodule/yellowpages/HomeActivity.java
+++ yellowpages/src/main/java/com/bdpqchen/yellowpagesmodule/yellowpages/HomeActivity.java
@@ -1,26 +1,48 @@
 package com.bdpqchen.yellowpagesmodule.yellowpages;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.SearchSuggestionsAdapter;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
+import com.bdpqchen.yellowpagesmodule.yellowpages.adapter.SearchResultsListAdapter;
 import com.bdpqchen.yellowpagesmodule.yellowpages.base.BaseActivity;
 import com.bdpqchen.yellowpagesmodule.yellowpages.fragment.FirstFragment;
 import com.bdpqchen.yellowpagesmodule.yellowpages.fragment.SecondFragment;
 import com.bdpqchen.yellowpagesmodule.yellowpages.fragment.ThirdFragment;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.orhanobut.logger.Logger;
 
 
 public class HomeActivity extends BaseActivity {
 
-//    @InjectView(R.id.toolbar)
+    private static final String TAG = "HomeActivity";
+    //    @InjectView(R.id.search_results_list)
+    private RecyclerView mSearchResultsList;
+//    @InjectView(R.id.parent_view)
+    private RelativeLayout mParentView;
+    //    @InjectView(R.id.toolbar)
     private Toolbar mToolbar;
+    private Context mContext;
+
+    private FloatingSearchView mSearchView;
+    private SearchResultsListAdapter mSearchResultsAdapter;
+
 
     @Override
     public int getLayout() {
@@ -30,13 +52,16 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected Toolbar getToolbar() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
         mToolbar.setTitle(getResources().getString(R.string.yp_app_name));
+
         return mToolbar;
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mContext = this;
         FirstFragment firstFragment = new FirstFragment();
         SecondFragment secondFragment = new SecondFragment();
         ThirdFragment thirdFragment = new ThirdFragment();
@@ -46,8 +71,196 @@ public class HomeActivity extends BaseActivity {
         fragmentTransaction.add(R.id.fragment_container_collected, secondFragment);
         fragmentTransaction.add(R.id.fragment_container_list, thirdFragment);
         fragmentTransaction.commit();
+        mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
+        mParentView = (RelativeLayout) findViewById(R.id.parent_view);
+        mSearchResultsList = (RecyclerView) findViewById(R.id.search_results_list);
 
+        setupSearchView();
+        setupResultsList();
 
+    }
+
+    private void setupSearchView() {
+
+        mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+
+                if (!oldQuery.equals("") && newQuery.equals("")) {
+                    mSearchView.clearSuggestions();
+                } else {
+
+                    //this shows the top left circular progress
+                    //you can call it where ever you want, but
+                    //it makes sense to do it when loading something in
+                    //the background.
+                    mSearchView.showProgress();
+
+                    //simulates a query call to a data source
+                    //with a new query.
+                    /*DataHelper.findSuggestions(getActivity(), newQuery, 5,
+                            FIND_SUGGESTION_SIMULATED_DELAY, new DataHelper.OnFindSuggestionsListener() {
+
+                                @Override
+                                public void onResults(List<ColorSuggestion> results) {
+
+                                    //this will swap the data and
+                                    //render the collapse/expand animations as necessary
+                                    mSearchView.swapSuggestions(results);
+
+                                    //let the users know that the background
+                                    //process has completed
+                                    mSearchView.hideProgress();
+                                }
+                            });*/
+                }
+
+                Logger.d(TAG, "onSearchTextChanged()");
+            }
+        });
+
+        mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+            @Override
+            public void onSuggestionClicked(final SearchSuggestion searchSuggestion) {
+
+                /*ColorSuggestion colorSuggestion = (ColorSuggestion) searchSuggestion;
+                DataHelper.findColors(getActivity(), colorSuggestion.getBody(),
+                        new DataHelper.OnFindColorsListener() {
+
+                            @Override
+                            public void onResults(List<ColorWrapper> results) {
+                                mSearchResultsAdapter.swapData(results);
+                            }
+
+                        });
+                mLastQuery = searchSuggestion.getBody();
+                */
+                Logger.d(TAG, "onSuggestionClicked()");
+            }
+
+            @Override
+            public void onSearchAction(String query) {
+                /*mLastQuery = query;
+
+                DataHelper.findColors(getActivity(), query,
+                        new DataHelper.OnFindColorsListener() {
+
+                            @Override
+                            public void onResults(List<ColorWrapper> results) {
+                                mSearchResultsAdapter.swapData(results);
+                            }
+
+                        });*/
+                Logger.d(TAG, "onSearchAction()");
+            }
+        });
+
+        mSearchView.setOnFocusChangeListener(new FloatingSearchView.OnFocusChangeListener() {
+            @Override
+            public void onFocus() {
+
+                //show suggestions when search bar gains focus (typically history suggestions)
+//                mSearchView.swapSuggestions(DataHelper.getHistory(getActivity(), 3));
+
+                Logger.d(TAG, "onFocus()");
+            }
+
+            @Override
+            public void onFocusCleared() {
+
+                //set the title of the bar so that when focus is returned a new query begins
+//                mSearchView.setSearchBarTitle(mLastQuery);
+
+                //you can also set setSearchText(...) to make keep the query there when not focused and when focus returns
+                //mSearchView.setSearchText(searchSuggestion.getBody());
+
+                Logger.d(TAG, "onFocusCleared()");
+            }
+        });
+
+        //handle menu clicks the same way as you would
+        //in a regular activity
+        mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
+            @Override
+            public void onActionMenuItemSelected(MenuItem item) {
+
+/*                if (item.getItemId() == R.id.action_change_colors) {
+
+                    mIsDarkSearchTheme = true;
+
+                    //demonstrate setting colors for items
+                    mSearchView.setBackgroundColor(Color.parseColor("#787878"));
+                    mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
+                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+                } else {
+
+                    //just print action
+                    Toast.makeText(getActivity().getApplicationContext(), item.getTitle(),
+                            Toast.LENGTH_SHORT).show();
+                }*/
+
+            }
+        });
+
+        mSearchView.setOnHomeActionClickListener(new FloatingSearchView.OnHomeActionClickListener() {
+            @Override
+            public void onHomeClicked() {
+
+                Logger.d(TAG, "onHomeClicked()");
+            }
+        });
+
+        mSearchView.setOnBindSuggestionCallback(new SearchSuggestionsAdapter.OnBindSuggestionCallback() {
+            @Override
+            public void onBindSuggestion(View suggestionView, ImageView leftIcon,
+                                         TextView textView, SearchSuggestion item, int itemPosition) {
+/*
+                ColorSuggestion colorSuggestion = (ColorSuggestion) item;
+
+                String textColor = mIsDarkSearchTheme ? "#ffffff" : "#000000";
+                String textLight = mIsDarkSearchTheme ? "#bfbfbf" : "#787878";
+
+                if (colorSuggestion.getIsHistory()) {
+                    leftIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(),
+                            R.drawable.ic_history_black_24dp, null));
+
+                    Util.setIconColor(leftIcon, Color.parseColor(textColor));
+                    leftIcon.setAlpha(.36f);
+                } else {
+                    leftIcon.setAlpha(0.0f);
+                    leftIcon.setImageDrawable(null);
+                }
+
+                textView.setTextColor(Color.parseColor(textColor));
+                String text = colorSuggestion.getBody()
+                        .replaceFirst(mSearchView.getQuery(),
+                                "<font color=\"" + textLight + "\">" + mSearchView.getQuery() + "</font>");
+                textView.setText(Html.fromHtml(text));
+*/
+            }
+
+        });
+
+        mSearchView.setOnSuggestionsListHeightChanged(new FloatingSearchView.OnSuggestionsListHeightChanged() {
+            @Override
+            public void onSuggestionsListHeightChanged(float newHeight) {
+                mSearchResultsList.setTranslationY(newHeight);
+            }
+        });
+
+    }
+
+    private void setupResultsList() {
+        mSearchResultsAdapter = new SearchResultsListAdapter();
+        mSearchResultsList.setAdapter(mSearchResultsAdapter);
+        mSearchResultsList.setLayoutManager(new LinearLayoutManager(mContext));
     }
 
 
@@ -61,22 +274,37 @@ public class HomeActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int i = item.getItemId();
         if (i == R.id.action_search) {
+            int duration = 200;
+            mSearchView.setVisibility(View.VISIBLE);
+            mSearchResultsList.setVisibility(View.VISIBLE);
+            mParentView.setVisibility(View.VISIBLE);
 
-            Intent intent = new Intent(this, SearchActivity.class);
-            this.startActivity(intent);
-            this.overridePendingTransition(R.anim.yp_activity_open, R.anim.yp_activity_close);
+//            YoYo.with(Techniques.ZoomInRight)
+            YoYo.with(Techniques.SlideInRight)
+                    .duration(duration).playOn(findViewById(R.id.floating_search_view));
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mSearchView.setSearchFocused(true);
+
+                }
+            }, duration);
+/*
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mSearchView.setSearchFocused(false);
+                    mSearchView.setVisibility(View.GONE);
+                    mSearchResultsList.setVisibility(View.GONE);
+                    mParentView.setVisibility(View.GONE);
+//                    YoYo.with(Techniques.SlideOutLeft).duration(200).playOn((findViewById(R.id.floating_search_view)));
+                }
+            }, 4000);*/
+
             Logger.d("clicked the toolbar");
         }
         return super.onOptionsItemSelected(item);
 
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-//        revealFromCoordinates(motionEvent.getRawX(), motionEvent.getRawY());
-
-        return false;
-
-    }
 }
